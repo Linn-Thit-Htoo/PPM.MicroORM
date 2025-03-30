c973aade9edf9533375e5c2db6a4ac5edfcd24aa
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-30 08:24:35 +0630

    -

--- PPM.MiniORM.TestConsoleApp/BlogQuery.cs
+++ PPM.MiniORM.TestConsoleApp/BlogQuery.cs
@@ -1,11 +1,10 @@
-﻿namespace PPM.MiniORM.TestConsoleApp
+﻿namespace PPM.MiniORM.TestConsoleApp;
+
+public class BlogQuery
 {
-    public class BlogQuery
-    {
-        public static string BlogListQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent, IsDeleted
+    public static string BlogListQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent, IsDeleted
 FROM Tbl_Blog WHERE IsDeleted = @IsDeleted";
 
-        public static string GetBlogByIdQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent, IsDeleted
+    public static string GetBlogByIdQuery { get; } = @"SELECT BlogId, BlogTitle, BlogAuthor, BlogContent, IsDeleted
 FROM Tbl_Blog WHERE BlogId = @BlogId AND IsDeleted = @IsDeleted";
-    }
 }
