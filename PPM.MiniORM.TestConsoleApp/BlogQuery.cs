6613705a25555d87381fb9bcb4afe788712acc55
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-30 08:24:30 +0630

    -

--- PPM.MiniORM.TestConsoleApp/BlogQuery.cs
+++ PPM.MiniORM.TestConsoleApp/BlogQuery.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PPM.MiniORM.TestConsoleApp
+﻿namespace PPM.MiniORM.TestConsoleApp
 {
     public class BlogQuery
     {
