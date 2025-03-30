4f519748e6b539db44ccd0e4f1731737a6e837f9
Linn Thit Htoo <lth1212001@gmail.com>
2025-03-30 09:05:16 +0630

    -

--- PPM.MiniORM.ConsoleApp/Program.cs
+++ PPM.MiniORM.ConsoleApp/Program.cs
@@ -371,6 +371,7 @@ public static class PPMMicroORM
             {
                 CommandType = commandType
             };
+
             if (parameters is not null)
             {
                 command.Parameters.AddRange(parameters.ToArray());
